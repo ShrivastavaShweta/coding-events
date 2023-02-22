7c8af964ea2e33ee117fa92eed316260baa0ead4
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-02-22 16:36:28 -0600

    added annotations for valiadations. Also added changes to create.html to handle error msgs

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -26,6 +26,7 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model){
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
@@ -35,7 +36,6 @@ public class EventController {
     public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
         if (errors.hasErrors()){
             model.addAttribute("title", "Create Event");
-            model.addAttribute("errorMsg", "Bad Data!");
             return "events/create";
         }
         EventData.add(newEvent);
